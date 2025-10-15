0a454a0507ba8a3df0479fcbb051074d99130c8a
Rafael Venancio <rafael.venancio08.06@gmail.com>
2025-10-15 15:25:05 -0300

    Manage Users Features Implemented

--- client/src/pages/ManageUsers/ManageUsers.jsx
+++ client/src/pages/ManageUsers/ManageUsers.jsx
@@ -1,16 +1,40 @@
 import './ManageUsers.css';
 import UserForm from '../../components/UserForm/UserForm.jsx';
 import UserList from '../../components/UserList/UserList.jsx';
+import { useEffect, useState } from 'react';
+import { fetchUsers } from '../../Service/UserService.js';
+import toast from 'react-hot-toast';
 
 
 const ManageUsers = () => {
+
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() =>{
+    async function loadUsers() {
+      try {
+          setLoading(true);
+          const response = await fetchUsers();
+          setUsers(response.data);
+
+      } catch (error) {
+          console.error("Error fetching users:", error);
+          toast.error("Failed to fetch users.");
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadUsers();
+  }, []);
+
   return (
     <div className="users-container text-light">
       <div className="left-column">
-        <UserForm />
+        <UserForm setUsers={setUsers}/>
       </div>
       <div className="right-column">
-        <UserList />
+        <UserList users={users} setUsers={setUsers} />
       </div>
     </div>
   )
