fb1a7501b1fff4c1cb27977be6d4f4216e29b209
Rafael Venancio <rafael.venancio08.06@gmail.com>
2025-10-15 14:38:48 -0300

    Login page & Login feature implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,29 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Menubar from "./components/Menubar/Menubar.jsx";
 import Dashboard from "./pages/Dashboard/Dashboard.jsx";
 import ManageCategory from "./pages/ManageCategory/ManageCategory.jsx"; 
 import ManageUsers from "./pages/ManageUsers/ManageUsers.jsx";
 import ManageItems from "./pages/ManageItems/ManageItems.jsx";
 import Explore from "./pages/Explore/Explore.jsx";
+import Login from "./pages/Login/Login.jsx";
 import { Toaster } from "react-hot-toast";
 
 
 
 const App = () => {
+  const location = useLocation();
   return (
     <div>
-        <Menubar />
+        {location.pathname !== "/login" && <Menubar />}
         <Toaster />
         <Routes>
             <Route path="/" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} /> 
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/category" element={<ManageCategory />} />
             <Route path="/users" element={<ManageUsers />} />
             <Route path="/items" element={<ManageItems />} />
-            <Route path="/explore" element={<Explore />} />
+            <Route path="/explore" element={<Explore />} /> 
         </Routes>
     </div>
   );
