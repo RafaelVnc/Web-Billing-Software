692cc7a4dd1c460a99843a995b0bedf6d9aa1a46
Rafael Venancio <rafael.venancio08.06@gmail.com>
2025-10-16 14:08:26 -0300

    Explore Page Categories & Items Integrated

---
+++ client/src/components/CartSummary/CartSummary.jsx
@@ -0,0 +1,10 @@
+import './CartSummary.css';
+
+
+const CartSummary = () => {
+  return (
+    <div>CartSummary</div>
+  )
+}
+
+export default CartSummary;
---
+++ client/src/components/CustomerForm/CustomerForm.jsx
@@ -0,0 +1,9 @@
+import './CustomerForm.css';
+
+const CustomerForm = () => {
+  return (
+    <div>CustomerForm</div>
+  )
+}
+
+export default CustomerForm;
